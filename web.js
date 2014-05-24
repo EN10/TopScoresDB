9ab3bea7f4744cacf1ca62f32d85fccef07b09d7
Enio <sfmneo@gmail.com>
2014-05-24 15:56:07 -0400

    sql

--- web.js
+++ web.js
@@ -1,20 +1,19 @@
 // https://github.com/brianc/node-postgres/wiki/Client
-var name = "enio";
-
 var express = require("express");
 var app = express();
 var pg = require('pg');
+var e;
 
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   client.query('INSERT INTO topscores VALUES ($1 , 100)',['Enio'],function(err, result) {
     done();
     if(err) return console.error(err);
-
-    app.get('/', function(req, res) {
-        res.send('Error: ' + err);
-    });
-
+    e = err;
   });
 });
 
+app.get('/', function(req, res) {
+    res.send('Error: ' + e);
+});
+    
 app.listen(80);
