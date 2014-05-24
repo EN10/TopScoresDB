fccffc11be09fad4b66470c52574d0977fb8d312
Enio <sfmneo@gmail.com>
2014-05-24 17:35:49 -0400

    err

--- web.js
+++ web.js
@@ -2,18 +2,17 @@
 var express = require("express");
 var app = express();
 var pg = require('pg');
-var e;
 
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   client.query('INSERT INTO topscores VALUES ($1 , 100)',['Enio'],function(err, result) {
     done();
     if(err) return console.error(err);
-    e = err;
+    
+    app.get('/', function(req, res) {
+    res.send('Error: ' + err);
+    });
+    
   });
 });
 
-app.get('/', function(req, res) {
-    res.send('Error: ' + e);
-});
-    
 app.listen(process.env.PORT);
