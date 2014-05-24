65d789557f5566a55f5692496162635447065b2b
Enio <sfmneo@gmail.com>
2014-05-24 07:39:07 -0400

    Procfile

--- web.js
+++ web.js
@@ -2,7 +2,7 @@
 var pg = require('pg');
 
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query("INSERT INTO films VALUES ('Enio', 100)", function(err, result) {
+  client.query('INSERT INTO films VALUES ("Enio", 100)', function(err, result) {
     done();
     if(err) return console.error(err);
     console.log(result.rows);
