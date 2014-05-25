2d000445e540e23ac2c771aee5ae2a65118517cf
Enio <sfmneo@gmail.com>
2014-05-25 08:39:20 -0400

    qus3

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var pg = require('pg');
 app.get('/', function(req, res) {
     if (req.query.u !== undefined && req.query.s !== undefined)
     {   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query('INSERT INTO topscores VALUES ($1 , $2)',req.query.u,req.query.s,function(err, result) {
+            client.query('INSERT INTO topscores VALUES ($1 , $2)',[req.query.u,req.query.s],function(err, result) {
             done();
             if(err) return console.error(err);
             
