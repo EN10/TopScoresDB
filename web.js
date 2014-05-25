d513f57ff8855ede1a6fc24a6a7dfa46e010b9f1
Enio <sfmneo@gmail.com>
2014-05-25 10:46:02 -0400

    rows

--- web.js
+++ web.js
@@ -7,14 +7,14 @@ var pg = require('pg');
 app.get('/', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function(err, client) {
         if (req.query.u !== undefined && req.query.s !== undefined)
-        {   client.query('INSERT INTO topscores VALUES ($1 , $2)',[req.query.u,req.query.s],function(err, result) {
-            });
+        {   client.query('INSERT INTO topscores VALUES ($1 , $2)',
+                [req.query.u,req.query.s],function(err, result) {});
         }        
         client.query('SELECT * FROM topscores',function(err, result) {
 /*      var ts ="";
         for (var i=0; result.rows.length; i++)
         {   ts += result.rows[i].toString();    }
-*/      res.end(result.rows[0].toString);
+*/      res.end(result.rows);
         //res.end(JSON.stringify(result.rows,null,'\t'));
         });
     });
