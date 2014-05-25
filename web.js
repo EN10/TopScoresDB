c37483069c71e7d456541171b98c327af1d42359
Enio <sfmneo@gmail.com>
2014-05-25 10:11:41 -0400

    output

--- web.js
+++ web.js
@@ -5,22 +5,18 @@ var app = express();
 var pg = require('pg');
 
 app.get('/', function(req, res) {
-    if (req.query.u !== undefined && req.query.s !== undefined)
-    {   try {
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query('INSERT INTO topscores VALUES ($1 , $2)',[req.query.u,req.query.s],function(err, result) {
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+        if (req.query.u !== undefined && req.query.s !== undefined)
+        {   client.query('INSERT INTO topscores VALUES ($1 , $2)',[req.query.u,req.query.s],function(err, result) {
             });
-            client.query('SELECT * FROM topscores',function(err, result) {
-            done();
-/*            var ts ="";
-            for (var i=0; result.rows.length; i++)
+        }        
+        client.query('SELECT * FROM topscores',function(err, result) {
+/*      var ts ="";
+        for (var i=0; result.rows.length; i++)
         {   ts += result.rows[i].toString();    }
-*/
-            res.end(JSON.stringify(result.rows));
-            });
+*/      res.end(JSON.parse(result.rows));
         });
-        } catch(err)    {res.send(err);}
-    }
+    });
 });
 
 app.listen(process.env.PORT);
