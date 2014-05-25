9fac6022ab0ca4bb19edaaf9b8d32911640c8ee0
Enio <sfmneo@gmail.com>
2014-05-25 09:19:35 -0400

    trycatch

--- web.js
+++ web.js
@@ -6,21 +6,16 @@ var pg = require('pg');
 
 app.get('/', function(req, res) {
     if (req.query.u !== undefined && req.query.s !== undefined)
-    {   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    {   try {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             client.query('INSERT INTO topscores VALUES ($1 , $2)',[req.query.u,req.query.s],function(err, result) {
             });
-            if (err !== null)   res.send('Error: ' + err);
-            
             client.query('SELECT * FROM topscores',function(err, result) {
             done();
-            if (err !== null)   res.send('Error: ' + err);
-            else {
-                var json = JSON.stringify(result.rows);
-                res.writeHead(200, {'content-type':'application/json', 'content-length':json.length}); 
-                res.end(json);
-                }
+            res.end(result.rows);
             });
         });
+        } catch(err)    {res.send(err);}
     }
 });
 
