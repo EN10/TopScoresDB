2ab20d34888dea5a12f39832988f0560fbc648ff
EN10 <sfm.neo@gmail.com>
2014-05-26 14:02:07 +0100

    add console err

--- web.js
+++ web.js
@@ -8,14 +8,15 @@ app.get('/', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function(err, client) {
         if (req.query.u !== undefined && req.query.s !== undefined)
         {   client.query('INSERT INTO topscores VALUES ($1 , $2)',
-                [req.query.u,req.query.s],function(err, result) {});
+                [req.query.u,req.query.s],function(err, result) {
+                    console.log(err);   });
         }        
         client.query('SELECT * FROM topscores ORDER BY score DESC',function(err, result) {
         var ts = "";
         for (var i=0; i < result.rows.length; i++)
         {   ts += result.rows[i].name+' : '+result.rows[i].score+'\n';  }
         res.end(ts);
-        });
+            console.log(err);  });
     });
 });
 
